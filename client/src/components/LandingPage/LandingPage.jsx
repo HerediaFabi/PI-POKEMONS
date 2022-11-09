4dfbd7796190901f2879c89e4672ffdba25bf60f
HerediaFabi <fabrilheredia@gmail.com>
2022-11-09 16:59:56 -0300

    css Landing done

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -5,15 +5,17 @@ import { Link } from "react-router-dom";
 const LandingPage = (props) => {
   return (
     <div className="landing">
+      <div className="mountain1"></div>
+      <div className="mountain2"></div>
       <div className="infoLanding">
         <div className="sectionLanding1">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png"
             alt=""
           />
-          <Link to="/home" id="btnLanding">
-            Comenzar
-          </Link>
+          <div id="btnLanding">
+            <Link to="/home">Comenzar</Link>
+          </div>
         </div>
         <div className="sectionLanding2">
           <img
@@ -21,7 +23,6 @@ const LandingPage = (props) => {
             alt=""
           />
         </div>
-        <div className="mountain"></div>
       </div>
     </div>
   );
